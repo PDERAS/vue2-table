5c7d85adc43a1ce1f25b5421139a2b66f1b04210
Cody Moorhouse <cody.moorhouse@pderas.com>
2018-03-15 10:11:57 -0600

    Update to work with I.E

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import Table from './components/Table.vue';
 import options from './options';
 
-export default {
-    install (Vue, globalOptions) {
-        if (globalOptions) {
-            Object.keys(globalOptions).map(function(key) {
-                options[key] = globalOptions[key];
-            });
-        }
-
-        Vue.component('vue-table', Table);
+function install (Vue, globalOptions) {
+    if (globalOptions) {
+        Object.keys(globalOptions).map(function(key) {
+            options[key] = globalOptions[key];
+        });
     }
+
+    Vue.component('vue-table', Table);
 }
+
+export default install;
